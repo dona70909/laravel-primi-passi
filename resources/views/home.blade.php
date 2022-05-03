4b6c448a3848142e5f8e21bfbeda270a3e29280c
Donatella7 <campaniellodonatella@gmail.com>
2022-05-03 14:55:50 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Welcome Laravel</h1>
+                </div>
+            </div>
+            
+        </section>
+    </header>
+</body>
+</html>
